5074e3906fe779c65b1bf382147097313aed14b6
JeffersonBatista <jefferson_tecnico@hotmail.com>
2021-10-05 17:29:00 -0300

    Programa Principal

--- src/teste_letras.kt
+++ src/teste_letras.kt
@@ -1,18 +1,11 @@
 fun main(){
     val entrada = readLine()!!;
     val listpalavras = entrada.split(" ");//Transforma a frase em um array de palavras
-    var resultado = "";
-    var palavra = "";
     for(i in listpalavras){
-        var listletras = i.toList();//Transforma cada palavra em um array de letras
-        var result = listletras.distinct();//Elimina letras duplicadas
-        for (i in listpalavras){//Para cada índice de palavra
-            resultado = palavra;
-            for (i in result){
-                val palavra = i;
-            }
-        }
-        print(resultado);
+        val listletras = i.toList();//Transforma cada palavra em um array de letras
+        val result = listletras.distinct();//Elimina letras duplicadas
+        print("$result");
         //print("$result");//oo ratoato roeuoeu aa roupaoupa dodo reiei dee romaoma -> o rato roeu a roupa do rei de roma
     }
+
 }
