d6a84f7f8af14fc566b6540b78595a881155d7b3
JeffersonBatista <jefferson_tecnico@hotmail.com>
2021-10-05 20:23:59 -0300

    Final Project

--- src/teste_letras.kt
+++ src/teste_letras.kt
@@ -1,11 +1,23 @@
+/*  Programa: Removedor de duplicata
+    Autor: Jefferson Batista
+    Versão: 1.0
+    Conclusão: 05/10/2021
+    Breve descrição:
+        Este programa tem como objetivo analisar uma frase
+        a fim de remover letras duplicadas de cada palavra.
+ */
+
 fun main(){
     val entrada = readLine()!!;
     val listpalavras = entrada.split(" ");//Transforma a frase em um array de palavras
+    var retorno = "";
     for(i in listpalavras){
         val listletras = i.toList();//Transforma cada palavra em um array de letras
-        val result = listletras.distinct();//Elimina letras duplicadas
-        print("$result");
-        //print("$result");//oo ratoato roeuoeu aa roupaoupa dodo reiei dee romaoma -> o rato roeu a roupa do rei de roma
+        val result = listletras.distinct(); //Elimina letras duplicadas
+        var palavra = result.joinToString("");//Transforma o array em uma String
+        palavra += " ";//Atribui cada palavra separada por espaço
+        retorno += palavra;
     }
-
+    retorno = retorno.trim() + ".";//Formata a string removendo espaços desnecessários
+    print(retorno)
 }
