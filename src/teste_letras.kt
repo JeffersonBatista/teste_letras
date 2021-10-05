dcf0c6912910f9b6b57aa3a7123cbd42efe75f99
JeffersonBatista <jefferson_tecnico@hotmail.com>
2021-10-05 14:54:26 -0300

    Sort words in arrays

--- src/teste_letras.kt
+++ src/teste_letras.kt
@@ -1,6 +1,10 @@
-
 fun main(){
     val entrada = readLine()!!;
-    val resposta = entrada;
-    println(resposta);
+    val listpalavras = entrada.split(" ");//Transforma a frase em um array de palavras
+    for(i in listpalavras){
+        val listletras = i.toList();//Transforma cada palavra em um array de letras
+        val result = listletras.distinct();//Elimina letras duplicadas
+        print("$result");//oo ratoato roeuoeu aa roupaoupa dodo reiei dee romaoma -> o rato roeu a roupa do rei de roma
+    }
+
 }
